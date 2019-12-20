631dc9f553a26cf454fe70d879745dbcdd847ad6
minpyaemoe <minpyaemoe@gmail.com>
2019-12-20 16:49:49 +0800

    removed parters and added inside about us

--- src/components/CurrentEvent.vue
+++ src/components/CurrentEvent.vue
@@ -5,85 +5,10 @@
                 <div class="text-center">
                     <h2 class="mb-8 mt-6 dark indigo--text" style="font-family: Graphik-Bold; font-size: 34px"><b>Agenda</b></h2>
                 </div>
-                <v-tabs class="mb-10" background-color="transparent" color="blue" centered>
-                    <v-tab>
-                        <h2 class="text--blue" style="font-family: Graphik-Bold; font-size: 24px"><b>Day 1</b></h2>
-                    </v-tab>
-                    <v-tab>
-                        <h2 class="text--blue" style="font-family: Graphik-Bold; font-size: 24px"><b>Day 2</b></h2>
-                    </v-tab>
-                    <v-tab-item>
-                        <v-container>
-                            <v-timeline :dense="$vuetify.breakpoint.smAndDown">
-                                <v-timeline-item
-                                        v-for="(item, i) in items"
-                                        :key="i"
-                                        :color="item.color"
-                                        :icon="item.icon"
-                                        fill-dot
-                                >
-                                    <template v-slot:opposite>
-                                        <span :class="`headline font-weight-bold ${item.color}--text`" v-text="item.time"></span>
-                                    </template>
-                                    <v-card :color="item.color" dark>
-                                        <v-card-title class="title"><span class="hidden-sm-and-up">{{item.time}}: &nbsp; </span>Lorem Ipsum Dolor</v-card-title>
-                                        <v-card-text class="white text--primary">
-                                            <p>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet
-                                                nec
-                                                ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim
-                                                zril
-                                                disputando voluptatibus, vix an salutandi sententiae.</p>
-                                            <v-btn :color="item.color" class="mx-0" outlined v-on="on">Button</v-btn>
-                                        </v-card-text>
-                                    </v-card>
-                                </v-timeline-item>
-                            </v-timeline>
-                        </v-container>
-                    </v-tab-item>
-                    <v-tab-item>
-                        <v-container>
-                            <v-timeline align-top :dense="$vuetify.breakpoint.smAndDown">
-                                <v-timeline-item
-                                        v-for="(item, i) in items"
-                                        :key="i"
-                                        :color="item.color"
-                                        :icon="item.icon"
-                                        fill-dot
-                                >
-                                    <template v-slot:opposite>
-                                        <span :class="`headline font-weight-bold ${year.color}--text`" v-text="item.time()"></span>
-                                    </template>
-                                    <v-card :color="item.color" dark>
-                                        <v-card-title class="title">Lorem Ipsum Dolor</v-card-title>
-                                        <v-card-text class="white text--primary">
-                                            <p>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet
-                                                nec
-                                                ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim
-                                                zril
-                                                disputando voluptatibus, vix an salutandi sententiae.</p>
-                                            <v-btn :color="item.color" class="mx-0" outlined v-on="on">Button</v-btn>
-                                        </v-card-text>
-                                    </v-card>
-                                </v-timeline-item>
-                            </v-timeline>
-                        </v-container>
-                    </v-tab-item>
-                </v-tabs>
-                <div class="text-center mt-n11">
-                    <h2 class="display-1 mb-8 dark indigo--text"><b>End</b></h2>
+                <div class="text-center" justify="center" style="top: 50%">
+                    <h2 class="dark black--text" style="font-family: Graphik-Bold; font-size: 48px"><b>Coming Soon!</b></h2>
                 </div>
             </v-layout>
-        <!--</template>-->
-        <!--<v-sheet class="text-center" height="200px">-->
-            <!--<v-btn-->
-                    <!--class="mt-6"-->
-                    <!--text-->
-                    <!--color="error"-->
-                    <!--@click="sheet = !sheet"-->
-            <!--&gt;close</v-btn>-->
-            <!--<div class="my-3">This is a bottom sheet using the inset prop</div>-->
-        <!--</v-sheet>-->
-    <!--</v-bottom-sheet>-->
 </template>
 
 <script>
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ import speakers from './components/Speakers';
 import landingpagehero from './components/LandingPageHero';
 import contactus from './components/ContactUs';
 import pricing from './components/Pricing';
-import partner from './components/Partners';
 
 const routes = [
     { path: '/', component: about },
@@ -13,7 +12,6 @@ const routes = [
     { path: '/landingpage', component: landingpagehero },
     { path: '/contact-us', component: contactus },
     { path: '/pricing', component: pricing},
-    { path: '/partners', component: partner},
 ];
 
 export default routes;
